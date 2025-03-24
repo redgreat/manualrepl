0eb56fd4e6bda5079ee40a9253a168bf8a30a2d6
wongcw <rubygreat@msn.com>
2025-03-24 13:33:30 +0800

    运行成功并添加阿里云镜像打包

--- src/main.py
+++ src/main.py
@@ -4,10 +4,13 @@
 # @generate at 2025/3/21 15:26
 # comment: 监听mysql binglog，监听变化量后写入宽表
 
+import datetime
+import decimal
 import sys
 import os
 import configparser
 from loguru import logger
+import json
 
 from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication.row_event import (
@@ -16,6 +19,9 @@ from pymysqlreplication.row_event import (
     WriteRowsEvent,
 )
 
+from supplier_info import SupplierProcessor
+from supplier_cust import CustProcessor
+
 # 数据库连接定义
 config = configparser.ConfigParser()
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -23,13 +29,20 @@ project_root = os.path.dirname(current_dir)
 config_path = os.path.join(project_root, "conf", "db.cnf")
 config.read(config_path)
 
-my_host = config.get("master", "host")
-my_database = config.get("master", "database").split(',')
-my_tables = config.get("master", "tables").split(',')
-my_user = config.get("master", "user")
-my_password = config.get("master", "password")
-my_port = int(config.get("master", "port"))
+src_host = config.get("source", "host")
+src_database = config.get("source", "database").split(',')
+src_tables = config.get("source", "tables").split(',')
+src_user = config.get("source", "user")
+src_password = config.get("source", "password")
+src_port = int(config.get("source", "port"))
+src_charset = config.get("source", "charset")
 
+tar_host = config.get("target", "host")
+tar_port = int(config.get("target", "port"))
+tar_database = config.get("target", "database")
+tar_user = config.get("target", "user")
+tar_password = config.get("target", "password")
+tar_charset = config.get("target", "charset")
 
 # 日志配置
 logDir = os.path.expanduser("../log/")
@@ -49,39 +62,88 @@ logger.add(
     level="INFO",
 )
 
-MYSQL_SETTINGS = {
-    "host": my_host,
-    "port": 3306,
-    "user": my_user,
-    "passwd": my_password,
-    "charset": "utf8",
+SRC_MYSQL_SETTINGS = {
+    "host": src_host,
+    "port": src_port,
+    "user": src_user,
+    "passwd": src_password,
+    "charset": src_charset,
 }
+
+TAR_MYSQL_SETTINGS = {
+    "host": tar_host,
+    "port": tar_port,
+    "database": tar_database,
+    "user": tar_user,
+    "passwd": tar_password,
+    "charset": tar_charset,
+}
+
+
+def dict_to_str(value):
+    if isinstance(value, datetime.datetime):
+        return value.strftime('%Y-%m-%d %H:%M:%S')
+    elif isinstance(value, decimal.Decimal):
+        return str(value)
+    elif isinstance(value, bytes):
+        return value.hex()
+    elif isinstance(value, str):
+        return value.strip("'")
+    elif isinstance(value, int):
+        return value
+    elif value is None:
+        # return 'NULL'
+        return None
+    else:
+        return f"'{value}'"
+
+
+def dict_to_json(res_value):
+    json_record = {}
+    for key, value in res_value.items():
+        if key not in ['schema', 'table', 'action']:
+            json_record[key] = dict_to_str(value)
+    return json.dumps(json_record, ensure_ascii=False, indent=4)
 
 
 def main():
     stream = BinLogStreamReader(
-        connection_settings=MYSQL_SETTINGS,
+        connection_settings=SRC_MYSQL_SETTINGS,
         server_id=3,
         blocking=True,  # 持续监听
         only_events=[DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent],  # 指定只监听某些事件
-        only_schemas=my_database,  # 指定只监听某些库（但binlog还是要读取全部滴）
+        only_schemas=src_database,  # 指定只监听某些库（但binlog还是要读取全部滴）
+        only_tables=src_tables,  # 指定监听某些表
     )
 
-    for binlog_event in stream:
-        for row in binlog_event.rows:
-            event = {"schema": binlog_event.schema, "table": binlog_event.table}
+    with SupplierProcessor(TAR_MYSQL_SETTINGS) as supplier_handler, CustProcessor(TAR_MYSQL_SETTINGS) as cust_handler:
+        for binlog_event in stream:
+            for row in binlog_event.rows:
+                event = {"schema": binlog_event.schema, "table": binlog_event.table}
 
-            if isinstance(binlog_event, DeleteRowsEvent):
-                event["action"] = "delete"
-                event.update(row["values"])
-            elif isinstance(binlog_event, UpdateRowsEvent):
-                event["action"] = "update"
-                event.update(row["after_values"])
-            elif isinstance(binlog_event, WriteRowsEvent):
-                event["action"] = "insert"
-                event.update(row["values"])
-            print(event)
-            sys.stdout.flush()
+                if isinstance(binlog_event, WriteRowsEvent):
+                    event["action"] = "replace"
+                    event.update(row["values"])
+                elif isinstance(binlog_event, UpdateRowsEvent):
+                    event["action"] = "replace"
+                    event.update(row["after_values"])
+                elif isinstance(binlog_event, DeleteRowsEvent):
+                    event["action"] = "delete"
+                    event.update(row["values"])
+
+                json_data = json.loads(dict_to_json(event))
+                if binlog_event.table == "supplier_info":
+                    supplier_handler.handle_event(
+                        action=event["action"],
+                        data=json_data
+                    )
+                elif binlog_event.table == "supplier_cust":
+                    cust_handler.handle_event(
+                        action=event["action"],
+                        data=json_data
+                    )
+
+    sys.stdout.flush()
 
     stream.close()
 
